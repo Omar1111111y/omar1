fec0838ca6ced3b4c6d27488e9cd11fff9934b83
Omar1111111y <106968928+Omar1111111y@users.noreply.github.com>
2022-06-22 23:56:27 +0300

    Update sss.py

--- sss.py
+++ sss.py
@@ -6,8 +6,7 @@ from batt import *
 bot = telebot.TeleBot(botmm)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-        bot.reply_to(message, """مرحبا فيك في بوت عمر ستايل حتس>
-👍👍😍😍😍😍😍😍😍😍😍""")
+        bot.reply_to(message, """مرحبا فيك في بوت Omar_Style""")
 #       m =bot.reply_to(message)
 #       if m == "omar":
 #               bot.reply_to(message,"ok ok")
